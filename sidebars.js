dea28c3616ed98616ba28c81f43839a0cfc78684
Joyce·Kirkland <69964292+JoyceKirkland@users.noreply.github.com>
2021-01-17 14:32:44 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
-    'RealSense 深度相机': ['doc1', 'doc2', 'doc3'],
-    'Ubuntu / Windows': ['mdx'],
+    'RealSense 深度相机': ['doc1', 'doc3'],
+    'Ubuntu / Windows': ['doc2'],
     '杂谈/生活':['如何安装踩坑驱动'],
   },
 };
