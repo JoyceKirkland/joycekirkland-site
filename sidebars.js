5b8cd4e024b7bd1cb0826bc36fb057f7990663d9
Joyce·Kirkland <69964292+JoyceKirkland@users.noreply.github.com>
2021-01-29 20:56:07 +0800

    Create sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -2,6 +2,6 @@ module.exports = {
   someSidebar: {
     'RealSense 深度相机': ['doc1', 'doc3'],
     'Ubuntu / Windows': ['doc2'],
-    '杂谈/生活':['如何安装踩坑驱动'],
+    '杂谈/生活':['如何安装踩坑驱动','一次科目二练习引发的思考'],
   },
 };
